993f031311a8279a4be16effdd97b4e57ef9572d
William Ramsay <william_ramsay@sfu.ca>
2025-10-04 16:13:19 -0700

    first commit

---
+++ storm-hacks-2025/src/components/findcards.jsx
@@ -0,0 +1,22 @@
+<InputGroup>
+        <InputGroupTextarea placeholder="Ask, Search or Chat..." />
+        <InputGroupAddon align="block-end">
+          <InputGroupButton
+            variant="outline"
+            className="rounded-full"
+            size="icon-xs"
+          >
+            <IconPlus />
+          </InputGroupButton>
+          
+          <InputGroupButton
+            variant="default"
+            className="rounded-full"
+            size="icon-xs"
+            disabled
+          >
+            <ArrowUpIcon />
+            <span className="sr-only">Send</span>
+          </InputGroupButton>
+        </InputGroupAddon>
+            </InputGroup>
