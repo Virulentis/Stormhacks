5ad0733564eb51b8a4f2083abe7ad5ef2fc8742c
Charlotte Gao <2329888972@qq.com>
2025-10-05 10:58:14 -0700

    Add complete C++ backend with scraper and dataset

---
+++ README.md
@@ -0,0 +1,5 @@
+# TGC Card Finder
+This is the repo made for the stormhacks 2025 event. It's a website that scrapes and aggregates TCG Card data (name, price, etc) from multiple online card shopping websites.
+
+# Diagram
+<img width="1464" height="1130" alt="image" src="https://github.com/user-attachments/assets/1b2a53e9-6ea4-43d4-947e-053c0a574f60" />
---
+++ storm-hacks-2025/frontend/src/App.tsx
@@ -0,0 +1,89 @@
+
+import './App.css'
+import { ThemeProvider } from "@/components/theme-provider"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+  InputGroupText,
+  InputGroupTextarea,
+} from "@/components/ui/input-group"
+import { Button } from "@/components/ui/button"
+import Hero from './components/Hero'
+import CardScrollArea  from './components/card-scroller'
+import AppSidebar from './components/app-sidebar'
+import { SidebarProvider, SidebarTrigger, SidebarInset } from "@/components/ui/sidebar"
+
+import { useState, useEffect } from 'react'
+import express from 'express';
+import redisClient from './redis.js';
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [showSidebar, setShowSidebar] = useState(false)
+
+  const [isSearchExpanded, setIsSearchExpanded] = useState(false);
+
+
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+  
+
+
+
+  <Hero />
+
+    
+    
+
+    
+  <div className="min-h-screen">
+  <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset className="flex flex-col">
+      {/* Header */}
+      <div className="w-full shadow-md border-b">
+        <div className="p-4 flex items-center gap-4">
+          <SidebarTrigger />
+          
+          <p>Card Finder</p>
+          <div id="find" className="flex-1">
+            <InputGroup>
+              <InputGroupInput placeholder="Enter Card here!" />
+              <InputGroupAddon align="inline-end">
+                <InputGroupButton variant="secondary">Search</InputGroupButton>
+              </InputGroupAddon>
+            </InputGroup>
+          </div>
+        </div>
+      </div>
+      
+      {/* Scrollable content area */}
+      <div className="flex-1 overflow-auto p-4">
+        <CardScrollArea />
+      </div>
+
+      
+
+    </SidebarInset>
+  </SidebarProvider>
+  </div>
+  <div className=''> </div>
+     
+     
+  
+
+
+    
+
+      
+
+    </ThemeProvider>
+  )
+}
+
+export default App
